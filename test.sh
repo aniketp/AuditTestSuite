77e086c0413f72f7c0cbe7df5e8e5d6b67807319
Aniket Pandey <aniketp@iitk.ac.in>
2018-02-06 01:33:53 +0530

    [Milestone] All 7 initial sockets passing

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ syscalls="socket(2) setsockopt(2) bind(2) listen(2)
 # Fetches the location of Audit trails
 # Default: /var/audit
 auditdir=$(cat ${audit_control} | grep "dir:" | cut -d ':' -f 2)
-echo "Audit Directory: ${auditdir}"
+echo "Audit Directory: ${auditdir} .. ✔"
 
 
 # Inserts auditd_enable="YES" in /etc/rc.conf
@@ -20,7 +20,7 @@ enable_audit()
 {
     sed -i "" '/auditd_enable/d' "$audit_daemon"
     echo "auditd_enable=\"YES\"" >> "$audit_daemon"
-    echo "Audit daemon configured"
+    echo "Audit daemon configured .. ✔"
     return
 }
 
@@ -28,8 +28,8 @@ enable_audit()
 # Set flags as network(nt) audit_class
 set_flag()
 {
-    sed -i "" '/\<flags:/s/\(.*\)/flags:nt/' "$audit_control"
-    echo "Associated flag set: (nt)"
+    sed -i "" '/\<flags:/s/\(.*\)/flags:lo,nt/' "$audit_control"
+    echo "Set Associated flag: (nt) .. ✔"
     return
 }
 
@@ -42,7 +42,7 @@ start_audit()
     local newt="audit -n"
 
     eval ${init}; eval ${newt}
-    echo "Audit daemon and new trail started"
+    echo "Audit daemon and new trail started .. ✔"
     return
 }
 
@@ -50,9 +50,9 @@ start_audit()
 # Stop Audit daemon
 stop_audit()
 {
-    local stop="audit -t"
+    local stop="audit -t &> /dev/null"
     eval ${stop}
-    echo "Audit daemon stopped"
+    echo "Audit daemon stopped .. ✔"
     return
 }
 
@@ -60,22 +60,19 @@ stop_audit()
 # Execute the network binary and connect using telnet
 launch_syscalls()
 {
-    #if [ -f "${PWD}/network" ]; then
-    #    echo "Please run 'make' first"
-    #fi
+    if [ ! -f ../audit/network ]; then
+        echo "Please run 'make' first .. ✘"
+        stop_audit
+        exit 1
+    fi
 
     # Launch network system calls
     ./network &
-    echo "launching system calls..."
-    #if [ ! $(./network &) ]
-    #then
-    #    echo "Failed to execute network binary"
-    #    exit 1
-    #fi
+    echo "launching system calls .. ✔"
 
     # Connect to the socket
     telnet localhost 9000 | echo "message"
-    echo "Connected via client"
+    echo "Connected via client .. ✔"
 
     return
 }
@@ -94,38 +91,36 @@ test_syscalls()
         echo "Testing ${syscall}.."
 
         praudit -l ${fullpath} | grep ${syscall} | while read -r find_syscall; do
-            if [ "$find_syscall" != "" ]; then
-                # Check for success and failure mode
-                check_success=$(echo ${find_syscall} | grep "return,success")
-                check_failure=$(echo ${find_syscall} | grep "return,failure")
+            # Check for success and failure mode
+            check_success=$(echo ${find_syscall} | grep "return,success")
+            check_failure=$(echo ${find_syscall} | grep "return,failure")
 
-                # Can add tests for arguments, file descriptors etc
+            # Can add tests for arguments, file descriptors etc
 
-                # Check if already tested both modes
-                if [ "$pass" = 1 ] && [ "$fail" = 1 ]; then
-                    break
-                fi
+            # Check if already tested both modes
+            if [ "$pass" = 1 ] && [ "$fail" = 1 ]; then
+                break
+            fi
 
-                if [ "$check_success" != "" ]; then
-                    echo "Success mode passed: ${syscall}"
-                    pass=1
-                fi
+            if [ "$check_success" != "" ] && [ "$pass_once" != 1 ]; then
+                echo "Success mode passed: ${syscall} .. ✔"
+                pass=1; pass_once=1
+            fi
 
-                if [ "$check_failure" != "" ]; then
-                    echo "Failure mode passed: ${syscall}"
-                    fail=1
-                fi
+            if [ "$check_failure" != "" ] && [ "$fail_once" != 1 ]; then
+                echo "Failure mode passed: ${syscall} .. ✔"
+                fail=1; fail_once=1
             fi
 
         done
 
         # Check if both modes passed
         if [ "$pass" = 0 ]; then
-            echo "Success mode failed: ${syscall}"
+            echo "Success mode failed: ${syscall} .. ✘"
         fi
 
         if [ "$fail" = 0 ]; then
-            echo "Failure mode failed: ${syscall}"
+            echo "Failure mode failed: ${syscall} .. ✘"
         fi
 
         # TODO: Print statistics
@@ -135,11 +130,19 @@ test_syscalls()
 }
 
 
+cleanup()
+{
+    echo '\nCleaning up test trails .. ✔'
+    rm -f "$auditdir/$1"
+    return
+}
+
+
 # Main function to launch all the functions above
 main()
 {
-#    enable_audit
-#    set_flag
+    enable_audit
+    set_flag
     start_audit
     launch_syscalls
 
@@ -147,9 +150,10 @@ main()
     local current_trail=$(ls ${auditdir} | grep ".not_terminated")
     local init_name=$(echo ${current_trail} | cut -d '.' -f 1)
     stop_audit
+
     local main_trail=$(ls ${auditdir} | grep ${init_name})
     test_syscalls ${main_trail}
-    # TODO: Implement cleanup
+    cleanup main_trail
 
     return
 }

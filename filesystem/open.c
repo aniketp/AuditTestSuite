d29606e899a7201e73c6fa1ff865ae48c61f72e4
Aniket Pandey <aniketp@freebsd.org>
2018-02-25 00:00:18 +0530

    Automate filesystem-read(fr) audit tests

--- filesystem/open.c
+++ filesystem/open.c
@@ -11,24 +11,24 @@ void main(){
     long int filedesc1, filedesc2;
 
     /* Success Condition: open(2) */
-    if ((filedesc1 = syscall(SYS_open, "test", O_RDWR)) == ERROR){
+    if ((filedesc1 = syscall(SYS_open, "/tmp/templog", O_RDWR)) == ERROR){
         perror("open");
         exit(ERROR);
     }
 
     /* Failure Condition: open(2) */
-    syscall(SYS_open, "ERROR", O_RDWR);
+    syscall(SYS_open, "/tmp/ERROR", O_RDWR);
 
 
     /* Success condition: openat(2) */
     /* Currently tests for read-write (no-create) flag */
-    if ((filedesc2 = openat(AT_FDCWD ,"test", O_RDWR)) == ERROR){
-        perror("open");
+    if ((filedesc2 = openat(AT_FDCWD ,"/tmp/templog", O_RDWR)) == ERROR){
+        perror("openat");
         exit(ERROR);
     }
 
     /* Failure Condition: openat(2) */
-    openat(AT_FDCWD ,"ERROR", O_RDWR);
+    openat(AT_FDCWD ,"/tmp/ERROR", O_RDWR);
 
 
     close((int) filedesc1);

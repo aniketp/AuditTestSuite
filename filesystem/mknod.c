3614008184bcc935ad1f489cec31dd01e8e7fd79
Aniket Pandey <aniketp@freebsd.org>
2018-03-02 01:46:28 +0530

    Add tests for mknod(2) and mknodat(2) Note: mknod(2) and mknodat(2) require superuser privilege Need to check if the user is root, else the test must be skipped

---
+++ filesystem/mknod.c
@@ -0,0 +1,34 @@
+/* REQUIRES SUPERUSER PRIVILEGE */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+
+#define ERROR (-1)
+
+void main(){
+    int filedesc1, filedesc2;
+    int filedesc = 0;
+    char *fifo1 = "/tmp/fifo1", *fifo2 = "/tmp/fifo2"
+    dev_t dev = 0;
+
+    /* Success Condition: mknod(2) */
+    if ((filedesc1 = mknod(fifo1, S_IFIFO | S_IRWXO, dev)) == ERROR){
+        perror("mknod");
+        exit(ERROR);
+    }
+
+    /* Failure condition: mknod(2) :: fifo already exists */
+    mknod(fifo1, S_IFIFO | S_IRWXO, dev);
+
+
+    /* Success condition: mknodat(2) */
+    if ((filedesc2 = mknodat(filedesc, fifo2, S_IFIFO | S_IRWXO, dev)) == ERROR){
+        perror("mknodat");
+        exit(ERROR);
+    }
+
+    /* Failure condition: mknodat(2) :: fifo already exists */
+    mknodat(filedesc, fifo2, S_IFIFO | S_IRWXO, dev);
+}

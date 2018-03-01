77c71d81071a8410963cd0a2ced6a6b96301eeb0
Aniket Pandey <aniketp@freebsd.org>
2018-03-02 00:45:32 +0530

    [Milestone] Tests for mkdir(2), mkdirat(2), mkfifo(2) & mkfifoat(2) Both Success and Failure mode

---
+++ filesystem/mkdir.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+
+#define ERROR (-1)
+
+void main(){
+    int filedesc1, filedesc2;
+    int filedesc = 0;
+    char *dir1 = "/tmp/temp1", *dir2 = "/tmp/temp2"
+    mode_t mode = 0777;
+
+    /* Success Condition: mkdir(2) */
+    if ((filedesc1 = mkdir(dir1, mode)) == ERROR){
+        perror("mkdir");
+        exit(ERROR);
+    }
+    /* Failure condition: mkdir(2) :: Directory already exists */
+    mkdir(dir1, mode);
+
+
+    /* Success condition: mkdirat(2) */
+    if ((filedesc2 = mkdirat(filedesc, file2, mode)) == ERROR){
+        perror("mkdirat");
+        exit(ERROR);
+    }
+
+    /* Failure condition: mkdirat(2) :: Directory already exists */
+    mkdirat(filedesc, file2, mode);
+}
---
+++ filesystem/mkfifo.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+
+#define ERROR (-1)
+
+void main(){
+    int filedesc1, filedesc2;
+    int filedesc = 0;
+    char *dir1 = "/tmp/temp1", *dir2 = "/tmp/temp2"
+    mode_t mode = 0777;
+
+    /* Success Condition: mkfifo(2) */
+    if ((filedesc1 = mkfifo(dir1, mode)) == ERROR){
+        perror("mkfifo");
+        exit(ERROR);
+    }
+    /* Failure condition: mkfifo(2) :: fifo already exists */
+    mkfifo(dir1, mode);
+
+
+    /* Success condition: mkfifoat(2) */
+    if ((filedesc2 = mkfifoat(filedesc, file2, mode)) == ERROR){
+        perror("mkfifoat");
+        exit(ERROR);
+    }
+
+    /* Failure condition: mkfifoat(2) :: fifo already exists */
+    mkfifoat(filedesc, file2, mode);
+}

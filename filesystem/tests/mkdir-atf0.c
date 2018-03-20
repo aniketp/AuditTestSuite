1760795b2ecd8c1790974c42357e9d5d090e94d2
Aniket Pandey <aniketp@iitk.ac.in>
2018-03-21 03:52:33 +0530

    Convert praudit(1) to libbsm(3) APIs for fetching audit record tokens

--- filesystem/tests/mkdir-atf0.c
+++ filesystem/tests/mkdir-atf0.c
@@ -18,10 +18,47 @@
 #define ERROR (-1)
 
 static void
-setup() {
+setup(void)
+{
     ATF_REQUIRE_EQ(0, system("service auditd onestatus || \
      { service auditd onestart && touch started_auditd ; }"));
 }
+
+static void
+getrecords(char *file1, char *file2)
+{
+    u_char *buff;
+    char *del = ",";
+    tokenstr_t token;
+    int reclen, bytesread;
+
+    FILE *fp1 = fopen(file1, "r");
+    FILE *fp2 = fopen(file2, "w");
+
+    /* Process the obtained BSM record, one token at a time */
+    while ((reclen = au_read_rec(fp1, &buff)) != ERROR) {
+        bytesread = 0;
+
+        /*
+        * Iterate through each BSM token, extracting the bits that are
+        * required to starting processing sequences.
+        */
+        while (bytesread < reclen) {
+            if (au_fetch_tok(&token, buff + bytesread, \
+                 reclen - bytesread) == ERROR) {
+                atf_tc_fail("Audit record incomplete");
+            };
+
+            /* Print the tokens as they are obtained, in their default form */
+            au_print_flags_tok(fp2, &token, del, AU_OFLAG_NONE);
+            bytesread += token.len;
+        }
+    }
+
+    free(buff);
+    fclose(fp1); fclose(fp2);
+}
+
 
 /*
  * Test1: mkdir(2) success
@@ -52,16 +89,15 @@ ATF_TC_BODY(mkdir_success, tc)
     fmask.am_success = class->ac_class;
     fmask.am_failure = class->ac_class;
 
-    /* Convert binary trail to human readable form (Temporary fix) */
-    snprintf(cmd, sizeof(cmd), "praudit -l %s > %s", file1, file2);
-
     /* Open /dev/auditpipe for auditing */
     fds[0].fd = open("/dev/auditpipe", O_RDONLY);
     fds[0].events = POLLIN;
     setup();
 
-    /* Check if the audit startup was properly logged */
-    /* If 'started_auditd' exists, that means we started auditd */
+    /*
+     * Check if the audit startup was properly logged in the trail
+     * If 'started_auditd' exists, that means we started auditd
+     */
     if (atf_utils_file_exists("started_auditd")) {
         if (poll(fds, 1, timeout) < 0) {
             atf_tc_fail("Poll: %s", strerror(errno));
@@ -75,7 +111,7 @@ ATF_TC_BODY(mkdir_success, tc)
                 fclose(fd);
 
                 /* We now have a proof that auditd(8) started smoothly */
-                ATF_REQUIRE(system(cmd) != ERROR);
+                getrecords(file1, file2);
             } else {
                 /* revents is not POLLIN */
                 atf_tc_fail("Auditpipe returned an unknown event"
@@ -94,17 +130,17 @@ ATF_TC_BODY(mkdir_success, tc)
      */
 
     /* Set local preselection mode for auditing */
-    if(ioctl(fds[0].fd, AUDITPIPE_SET_PRESELECT_MODE, &fmode) < 0){
+    if (ioctl(fds[0].fd, AUDITPIPE_SET_PRESELECT_MODE, &fmode) < 0) {
         atf_tc_fail("Preselection mode: %s", strerror(errno));
     }
 
     /* Set local preselection flag as (fc) for mkdir(2) */
-    if(ioctl(fds[0].fd, AUDITPIPE_SET_PRESELECT_FLAGS, &fmask) < 0){
+    if (ioctl(fds[0].fd, AUDITPIPE_SET_PRESELECT_FLAGS, &fmask) < 0) {
         atf_tc_fail("Preselection flag: %s", strerror(errno));
     }
 
     /* This removes any outstanding record on audit pipe */
-    if(ioctl(fds[0].fd, AUDITPIPE_FLUSH) < 0){
+    if (ioctl(fds[0].fd, AUDITPIPE_FLUSH) < 0) {
         atf_tc_fail("Auditpipe flush: %s", strerror(errno));
     }
 
@@ -119,12 +155,12 @@ ATF_TC_BODY(mkdir_success, tc)
      * Loop until the auditpipe returns something, check if it is what
      * we want else repeat the procedure until poll(2) times out.
      */
-    while(true){
+    while(true) {
         /* Update the current time left for auditpipe to return any event */
         ATF_REQUIRE_EQ(0, clock_gettime(CLOCK_MONOTONIC, &curptr));
         curptr.tv_sec = endptr.tv_sec - curptr.tv_sec;
 
-        switch(ppoll(fds, 1, &curptr, NULL)){
+        switch(ppoll(fds, 1, &curptr, NULL)) {
             /* ppoll(2) returns an event, check if it's the event we want */
             case 1:
                 if (fds[0].revents & POLLIN) {
@@ -134,8 +170,8 @@ ATF_TC_BODY(mkdir_success, tc)
                     fwrite(buff, 1, ret, fd);
                     fclose(fd);
 
-                    ATF_REQUIRE(system(cmd) != ERROR);
-                    if(atf_utils_grep_file("%s", file2, path)){
+                    getrecords(file1, file2);
+                    if (atf_utils_grep_file("%s", file2, path)) {
                         /* We have confirmed mkdir(2)'s audit */
                         atf_tc_pass();
                     }
@@ -169,6 +205,5 @@ ATF_TC_CLEANUP(mkdir_success, tc)
 ATF_TP_ADD_TCS(tp)
 {
     ATF_TP_ADD_TC(tp, mkdir_success);
-
     return atf_no_error();
 }

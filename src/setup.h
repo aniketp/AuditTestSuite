8d6c2e63b99fedf58cba0413cd6063f494d6f719
Aniket Pandey <aniketp@freebsd.org>
2018-05-02 19:59:37 +0530

    Separate the audit helper functions into setup.c source file

---
+++ src/setup.h
@@ -0,0 +1,50 @@
+/*-
+ * Copyright 2018 Aniket Pandey
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+
+#ifndef _SETUP_H_
+#define _SETUP_H_
+
+#ifndef ERROR
+#define ERROR (-1)
+#endif  /* ERROR */
+
+#ifndef BUFFLEN
+#define BUFFLEN 1024
+#endif  /* BUFFLEN */
+
+bool get_records(const char *, FILE *);
+void set_preselect_mode(int, au_mask_t *);
+void check_audit_startup(struct pollfd [], FILE *);
+void check_audit(struct pollfd [], const char *, FILE *);
+au_mask_t get_audit_class(const char *);
+FILE *setup(struct pollfd [], const char *);
+
+#endif  /* _SETUP_H_ */
+
+//TODO: some extern declarations

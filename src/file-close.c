5ed9037bee8e04ab69cc0e248c61b871428638d8
Aniket Pandey <aniketp@FreeBSD.org>
2018-06-08 02:39:24 +0530

    Remove 'All Rights Reserved' from all source/header files
    As mentioned by @bsdimp in https://reviews.freebsd.org/D15264
    Also in @shivansh /smoketestsuite: https://github.com/shivansh/smoketestsuite/pull/26

--- src/file-close.c
+++ src/file-close.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright 2018 Aniket Pandey
+ * Copyright (c) 2018 Aniket Pandey
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions

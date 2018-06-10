96b26aa2cd9e91a92d1268d2710617d69d171707
Aniket Pandey <aniketp@FreeBSD.org>
2018-06-11 02:58:39 +0530

    Replace redundant input_dir with the standard 'atf_get_srcdir' function

--- praudit/praudit_test.sh
+++ praudit/praudit_test.sh
@@ -25,42 +25,41 @@
 # $FreeBSD$
 #
 
-input_dir="$(atf_get_srcdir)/input"
 
 atf_test_case praudit
 praudit_body()
 {
 	# Check that comma delimiter is present with -d "," cmd
-	atf_check -s exit:1 -o file:"$input_dir"/del_comma \
-			praudit -d "," "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/del_comma \
+			praudit -d "," $(atf_get_srcdir)/trail
 
 	# Check that underscore delimiter is present with -d "_" cmd
-	atf_check -s exit:1 -o file:"$input_dir"/del_underscore \
-			praudit -d "_" "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/del_underscore \
+			praudit -d "_" $(atf_get_srcdir)/trail
 
 	# Check that praudit outputs default form without arguments
-	atf_check -s exit:1 -o file:"$input_dir"/no_args \
-			praudit "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/no_args \
+			praudit $(atf_get_srcdir)/trail
 
 	# Check that praudit outputs the numeric form with "-n" flag
-	atf_check -s exit:1 -o file:"$input_dir"/numeric_form \
-			praudit -n "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/numeric_form \
+			praudit -n $(atf_get_srcdir)/trail
 
 	# Check that praudit outputs the raw form with "-r" flag
-	atf_check -s exit:1 -o file:"$input_dir"/raw_form \
-			praudit -r "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/raw_form \
+			praudit -r $(atf_get_srcdir)/trail
 
 	# Check that praudit outputs the trail in same line with "-l" flag
-	atf_check -s exit:1 -o file:"$input_dir"/same_line \
-			praudit -l "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/same_line \
+			praudit -l $(atf_get_srcdir)/trail
 
 	# Check that praudit outputs the short form with "-s" flag
-	atf_check -s exit:1 -o file:"$input_dir"/short_form \
-			praudit -s "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/short_form \
+			praudit -s $(atf_get_srcdir)/trail
 
 	# Check that praudit outputs the XML file with "-x" flag
-	atf_check -s exit:1 -o file:"$input_dir"/xml_form \
-			praudit -x "$input_dir"/trail
+	atf_check -s exit:1 -o file:$(atf_get_srcdir)/xml_form \
+			praudit -x $(atf_get_srcdir)/trail
 }
 
 atf_init_test_cases()
